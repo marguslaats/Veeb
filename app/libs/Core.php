84b10bcda02050e74918d8a7b5f4bebf4721a164
marguslaats <margus.laats@khk.ee>
2020-02-19 11:12:13 +0200

    Esimene Commit

--- app/libs/Core.php
+++ app/libs/Core.php
@@ -3,32 +3,48 @@
 
 class Core
 {
-  protected $currentController = 'Pages';
-  protected $currentMethod = 'index';
+  protected $currentController = "Pages";
+  protected $currentMethod = "index";
   protected $params = array();
+
 
   public function __construct()
   {
-
     $url = $this->getUrl();
-    // Set up controller
+    echo '<pre>';
+    print_r($url);
+    echo '</pre>';
     if(file_exists('../app/controllers/'.ucwords($url[0]).'.php')){
       $this->currentController = ucwords($url[0]);
+      unset($url[0]);
+
+
     }
-    unset($url[0]);
+    // create controller object
+    require_once '../app/controllers/'.$this->currentController.'.php';
+    $this->currentController = new $this->currentController();
+    // check the method
+    if(isset($url[1])){
+      if(method_exists($this->currentController, $url[1])){
+        $this->currentMethod = $url[1];
+      }
+    }
     echo '<pre>';
     print_r($url);
     echo '</pre>';
 
+
+    echo '<pre>';
+    print_r($this->currentController);
+    echo '</pre>';
   }
 
-  // get url from link and prepare for use
   public function getUrl(){
     if(isset($_GET['url'])){
-      $url = rtrim($_GET['url'],  '/');
-      $url = filter_var($url, FILTER_SANITIZE_URL);
+      $url = rtrim($_GET['url'],'/');
+      $url = filter_var($url,FILTER_SANITIZE_URL);
       $url = explode('/', $url);
       return $url;
-    }
+    };
   }
 }
