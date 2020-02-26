9a378a095730e7a23a43ec4ae1f5d48fb56aedc8
marguslaats <margus.laats@khk.ee>
2020-02-26 09:42:33 +0200

    Esimene Commit

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -8,6 +8,43 @@ class Users extends Controller
   }
 
   public function register(){
-    $this->view('users/register');
+    // check post request
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+      // process form
+      $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+      // init data
+      $data = array(
+        'name' => trim($_POST['name']),
+        'email' => trim($_POST['email']),
+        'pass' => trim($_POST['pass']),
+        'pass2' => trim($_POST['pass2']),
+        'name_err' => '',
+        'email_err' => '',
+        'pass_err' => '',
+        'pass2_err' => ''
+      );
+      // validate name
+      if(empty($data['name'])){
+        $data['name_err'] = 'Please enter the name';
+      }
+      // validate email
+      if(empty($data['email'])){
+        $data['email_err'] = 'Please enter the Email';
+      }
+      // validate pass
+      if(empty($data['pass'])){
+        $data['pass_err'] = 'Please enter the Password';
+      }
+      // validate pass2
+      if(empty($data['pass2'])){
+        $data['pass2_err'] = 'Please enter the Password';
+      }
+    //  echo '<pre>';
+      //print_r($data);
+     // echo '</pre>';
+      $this->view('users/register', $data);
+    } else {
+      $this->view('users/register');
+    }
   }
 }
--- app/views/users/login.php
+++ app/views/users/login.php
@@ -1,3 +1,23 @@
 <?php require_once APPROOT.'/views/inc/header.php';?>
-  login
+  <!-- Login Form -->
+  <div class="row">
+    <div class="col-md-6 mx-auto">
+      <div class="card card-body bg-light mt-5">
+        <h2> Log In</h2>
+        <form action="<?php echo URLROOT;?>users/login" method="post">
+              <label for="email">E-mail: </label>
+              <input type="text" id="email" class="form-control">
+              <div class="form-group">
+                <label for="password">Password: </label>
+                <input type="password" id="password" class="form-control">
+                <br>
+                <div class="row">
+                  <div class="col">
+                    <input type="submit" value="Login" class="btn btn-block btn-info">
+                  </div>
+                </div>
+        </form>
+      </div>
+    </div>
+  </div>
 <?php require_once APPROOT.'/views/inc/footer.php';?>
