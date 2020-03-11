1709bc52834335e3f2798e14520ca9bcb6708e7e
marguslaats <margus.laats@khk.ee>
2020-03-11 09:47:34 +0200

    Postitused Võimalik vaadata

--- app/views/posts/show.php
+++ app/views/posts/show.php
@@ -1,5 +1,5 @@
 <?php require_once APPROOT.'/views/inc/header.php';?>
-    <a href="<?php echo URLROOT?>/posts" class="btn btn-dark btn-lg">Go Back</a>
+    <a href="<?php echo URLROOT?>/posts" class="btn btn-light">Back</a>
     <h1><?php echo $data['post']->title; ?></h1>
     <div class="bg-secondary text-white p-2 mb-3">
         Written by <?php echo $data['user']->name;?> on <?php echo $data['post']->created_at;?>
